75ed76b17446427c5d60dbb1cfe0b70ff8ac910e
lixin <lx19900310@163.com>
2016-07-12 16:51:10 +0800

    commit page

--- src/main/java/com/lx/test/SpringBootRun.java
+++ src/main/java/com/lx/test/SpringBootRun.java
@@ -3,21 +3,54 @@ package com.lx.test;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.MultipartConfigFactory;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+import javax.annotation.Generated;
+import javax.servlet.MultipartConfigElement;
 
 /**
  * Created by lx199 on 2016/5/12.
  */
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
-//@MapperScan("com.lx.test.repository")
-public class SpringBootRun {
+@SpringBootApplication
+@MapperScan("com.lx.test.repository")
+public class SpringBootRun extends SpringBootServletInitializer implements EmbeddedServletContainerCustomizer {
 
+
+
+    @Override
+    public SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(SpringBootRun.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(SpringBootRun.class,args);
     }
+//    @Bean
+//    public MultipartConfigElement multipartConfigElement() {
+//        MultipartConfigFactory factory = new MultipartConfigFactory();
+//        factory.setMaxFileSize("512000KB");
+//        factory.setMaxRequestSize("512000KB");
+//        return factory.createMultipartConfig();
+//    }
 
+//    @Bean
+//    public CommonsMultipartResolver multipartResolver() {
+//        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+//        multipartResolver.setDefaultEncoding("UTF-8");
+//        return multipartResolver;
+//    }
 
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer configure) {
+        configure.setPort(8585);
+    }
 }
--- src/test/java/com/lx/test/junit/TestSpringBoot.java
+++ src/test/java/com/lx/test/junit/TestSpringBoot.java
@@ -1,37 +1,37 @@
-package com.lx.test.junit;
-
-import com.lx.test.SpringBootRun;
-import com.lx.test.model.mybatis.User;
-import com.lx.test.repository.UserMapper;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-/**
- * Created by lx199 on 2016/6/3.
- */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(SpringBootRun.class)
-public class TestSpringBoot{
-
-    @Autowired
-    private UserMapper userMapper;
-
-    @Test
-    public void test1(){
-        User user = userMapper.selectByPrimaryKey(1);
-        Assert.assertNotNull(user);
-
-    }
-
-    @Test
-    public void test2(){
-        User user = new User(2,"shengjuan",26,1,366.66f);
-        userMapper.insert(user);
-    }
-
-
-}
+//package com.lx.test.junit;
+//
+//import com.lx.test.SpringBootRun;
+//import com.lx.test.model.mybatis.User;
+//import com.lx.test.repository.UserMapper;
+//import org.junit.Assert;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.SpringApplicationConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+///**
+// * Created by lx on 2016/6/3.
+// */
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(SpringBootRun.class)
+//public class TestSpringBoot{
+//
+//    @Autowired
+//    private UserMapper userMapper;
+//
+//    @Test
+//    public void test1(){
+//        User user = userMapper.selectByPrimaryKey(1);
+//        Assert.assertNotNull(user);
+//
+//    }
+//
+//    @Test
+//    public void test2(){
+//        User user = new User(2,"shengjuan",26,1,366.66f);
+//        userMapper.insert(user);
+//    }
+//
+//
+//}
